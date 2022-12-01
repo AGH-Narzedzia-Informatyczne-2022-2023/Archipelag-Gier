423d91d0df360682fc560540485d51f23197a976
jGrzyb <jgrzyb@student.agh.edu.pl>
2022-12-01 11:57:08 +0100

    konflikt2

--- SNAKE/snake-v0.1.cpp
+++ SNAKE/snake-v0.1.cpp
@@ -14,7 +14,7 @@ int main()
     place();
     for(int i=0; i<21; i++)
     for(int j=0; j<21; j++)
-    cout << i << " : " << j << " " << board[i][j] << "   ###  \n";
+    cout << i << " : " << j << " " << board[i][j] << "   ####  \n";
     return 0;
 }
 
